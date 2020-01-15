40fba78a4c5b7944f37c15cec60237a40da78e9a
samidahlawi <o.t5@hotmail.com>
2020-01-15 16:43:19 +0300

    Updates

---
+++ precache-manifest.cc9bc6c48b7a70ae24af68815310a431.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "738645cf807a9ca88565616cf3c1a04b",
+    "url": "/project3/index.html"
+  },
+  {
+    "revision": "507c3c2720510e9eae47",
+    "url": "/project3/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b8a6fd6aed9f8bd33fce",
+    "url": "/project3/static/js/2.c7e553c4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/project3/static/js/2.c7e553c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "507c3c2720510e9eae47",
+    "url": "/project3/static/js/main.110ef567.chunk.js"
+  },
+  {
+    "revision": "a989969ad070f75e945e",
+    "url": "/project3/static/js/runtime-main.6755828a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project3/static/media/logo.5d5d9eef.svg"
+  }
+]);
